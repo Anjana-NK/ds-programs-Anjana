c928c9f82b89380707dbd994c3d5e3328faf939e
Anjana-NK <anjana83nk@gmail.com>
2026-09-08 12:01:53 +0530

    added program for inserting and displaying linked list

---
+++ linkedlist.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+class linkedlist{
+    private:
+    struct node{
+        int data;
+        node *link;
+    }*start,*newnode,*temp;
+    public:
+    linkedlist();
+    void insert();
+    void display();
+};
+linkedlist::linkedlist(){
+    start =NULL;
+}
+void linkedlist::insert(){
+    int num;
+    char ch='y';
+    do
+    {
+        cout<<"enter the number ";
+        cin>>num;
+        newnode=new node;
+        newnode->data=num;
+        newnode->link=NULL;
+        if (start==NULL){
+            start=temp=newnode;
+        
+        }
+        else{
+            temp->link=newnode;
+            temp=newnode;
+        }
+        cout<<"do you want to continue ";
+        cin>>ch;
+
+    } 
+    while (ch=='y');
+    
+}
+void linkedlist::display()
+{
+    temp=start;
+    if(start==NULL){
+        cout<<"linked list is empty \n";
+    }
+    else{
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<"\n";
+            temp=temp->link;
+        }
+        cout<<"end ";
+    }
+}
+int main()
+{
+    linkedlist obj;
+    //clrscr();
+    //obj.display();
+    obj.insert();
+    obj.display();
+    //getch();
+    return 0;
+}
